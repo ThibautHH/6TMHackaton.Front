113e146859e13880f415a56b90988f8ecf0b15a3
LouisLanganay <louis.langanay@epitech.eu>
2024-01-15 10:35:02 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './src/**/**/*.{js,jsx,ts,tsx}'
+];
+export const theme = {
+  colors: {
+  },
+  fontFamily: {
+  },
+  extend: {
+    scale: {
+      99: '.99',
+      101: '1.01'
+    }
+  }
+};
+export const plugins = [];
