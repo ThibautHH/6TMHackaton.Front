f7cd604e973aa9599abbbf325204b0dc2c40cae1
Blaizot <guillaume.blaizot@epitech.eu>
2024-01-15 11:27:03 +0100

    feat: components

---
+++ src/components/PersonCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Person from '../utils/types/';
+
+const PersonCard: React.FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default PersonCard;
