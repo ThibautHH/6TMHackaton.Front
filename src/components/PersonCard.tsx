168c0179788b264553d9b8448275082b3373951d
LouisLanganay <louis.langanay@epitech.eu>
2024-01-15 12:06:52 +0100

    feat: Add hover effect to PersonCard component
    - Add useState hook to manage hover state
    - Add onMouseEnter and onMouseLeave event handlers to toggle hover state
    - Update image source based on hover state

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Person} from '../utils/types/';
 
 const PersonCard: React.FC<{person: Person}> = ({ person }) => {
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <div className='ml-5 '>
-      {person.nom}
+    <div
+      className='w-52 border-2 border-black rounded-lg overflow-hidden'
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <img
+        src={isHover ? person.photo_fun : person.photo_pro}
+        alt={person.nom}
+      />
     </div>
   );
 };
